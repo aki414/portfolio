293c2246ab53934eac2be8870442471e3cbf0a3e
Aki <akremsedki21@gmail.com>
2026-08-09 10:16:06 +0100

    Deploy site

--- src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <span>© {new Date().getFullYear()} Akrem Sedki</span>
-      <a href="#top">Back to top ↑</a>
-    </footer>
-  );
-}
